819b3f6af8f740f89a9a64e895a244524329b3a4
Ihor <uvik@uviks-MacBook-Pro.local>
2021-07-26 17:19:36 +0300

    example commit

--- books/models.py
+++ books/models.py
@@ -10,6 +10,7 @@ class Book(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
     def __str__(self):
         return f'{self.title}'
 
